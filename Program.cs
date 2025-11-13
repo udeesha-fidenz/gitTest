e332d29b833263a6cc3d39af7e73e32aceee66f4
udeesha-fidenz <udeesha.g@fidenz.com>
2025-11-13 12:03:11 +0530

    Fix:Missing colon added

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 class Program{
     static void Main(string[] args){
         Console.WriteLine("Hello, Fidenzz!!!");
-        Console.WriteLine("This is a new text to test the revert command!!!")
+        Console.WriteLine("This is a new text to test the revert command!!!");
         
     }
 }
